458a936b346433e0eea8a605c2c313aa829d3868
andrei-p-buculea <83306512+andrei-p-buculea@users.noreply.github.com>
2021-04-15 08:38:57 +0300

    TicTacToe
    A simple TicTacToe game.

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Andrei-Petrisor Buculea on 13.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
